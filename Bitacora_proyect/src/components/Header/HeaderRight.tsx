11e4497ac0238f0426d284cdd62dd01e60bdab2e
Rafael93r <150625382+Rafael93r@users.noreply.github.com>
2024-12-30 07:02:17 -0500

    nada de nada

--- Bitacora_proyect/src/components/Header/HeaderRight.tsx
+++ Bitacora_proyect/src/components/Header/HeaderRight.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Clock, Maximize, Minimize } from 'react-feather';
+import { Maximize, Minimize } from 'react-feather';
 
 const HeaderRight = () => {
     const [isFullScreen, setIsFullScreen] = useState(false);
@@ -27,7 +27,7 @@ const HeaderRight = () => {
 
             <div className="dropdown me-1">
                 <button className="btn btn-link text-secondary p-2" data-bs-toggle="dropdown">
-                    <Clock size={25} />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2"> <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"></path> <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path> </svg> 
                     <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill" style={{ backgroundColor: '#f6952c', fontSize: '0.7em' }}>
                         10
                     </span>
--- Bitacora_proyect/src/components/Sidebar/Sidebar.tsx
+++ Bitacora_proyect/src/components/Sidebar/Sidebar.tsx
@@ -4,11 +4,34 @@ import { Button } from "react-bootstrap";
 const Sidebar: React.FC = () => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-  const navItems = [
-    { label: "Reportes", icon: "M12 9v4 M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z M12 16h.01" },
-    { label: "Farmacias", icon: "M3 21l18 0 M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16 M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4 M10 9l4 0 M12 7l0 4" },
-    { label: "Proveedores", icon: "M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0 M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2 M16 3.13a4 4 0 0 1 0 7.75 M21 21v-2a4 4 0 0 0 -3 -3.85" },
+  const navItems1 = [
+    {
+      label: "Reportes",
+      icon: "M12 9v4 M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z M12 16h.01",
+    },
+    {
+      label: "Farmacias",
+      icon: "M3 21l18 0 M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16 M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4 M10 9l4 0 M12 7l0 4",
+    },
+    {
+      label: "Proveedores",
+      icon: "M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0 M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2 M16 3.13a4 4 0 0 1 0 7.75 M21 21v-2a4 4 0 0 0 -3 -3.85",
+    },
+  ];
 
+  const navItems2 = [
+    {
+      label: "Personal",
+      icon: "M12 9v4 M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0z M12 16h.01",
+    },
+    {
+      label: "Farmacias",
+      icon: "M3 21l18 0 M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16 M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4 M10 9l4 0 M12 7l0 4",
+    },
+    {
+      label: "Proveedores",
+      icon: "M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0 M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2 M16 3.13a4 4 0 0 1 0 7.75 M21 21v-2a4 4 0 0 0 -3 -3.85",
+    },
   ];
 
   return (
@@ -21,11 +44,45 @@ const Sidebar: React.FC = () => {
               INTERNET
             </div>
 
-            {navItems.map((item, index) => (
+            {navItems1.map((item, index) => (
               <li className="nav-item" key={index}>
                 <a
                   href="#"
-                  className="nav-link d-flex align-items-center gap-2"
+                  className="nav-link d-flex align-items-center gap-2 py-2"
+                  style={{
+                    color: hoveredIndex === index ? "orange" : "black",
+                    cursor: "pointer",
+                  }}
+                  onMouseEnter={() => setHoveredIndex(index)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    width="24"
+                    height="24"
+                    strokeWidth="2"
+                  >
+                    <path d={item.icon}></path>
+                  </svg>
+                  {item.label}
+                </a>
+              </li>
+            ))}
+            <br />
+            <div className="sidebar__section-heading d-block my-2 fw-semibold text-capitalize ls-wider fs-sm">
+              INVENTARIO
+            </div>
+
+            {navItems2.map((item, index) => (
+              <li className="nav-item" key={index}>
+                <a
+                  href="#"
+                  className="nav-link d-flex align-items-center gap-2 py-2"
                   style={{
                     color: hoveredIndex === index ? "orange" : "black",
                     cursor: "pointer",
@@ -51,20 +108,21 @@ const Sidebar: React.FC = () => {
               </li>
             ))}
           </ul>
-        </div>
-        <div className="border-top p-3 position-fixed bottom-0 w-100">
-          <div className="d-flex align-items-center justify-content-between">
-            <div className="d-flex align-items-center">
-              <i className="bi bi-person-circle fs-4 me-2"></i>
-              <span className="fw-bold">rafa</span>
+          <br />
+          <div className="p-3 bottom-0 w-100" style={{ backgroundColor: "#f8f9fa" }}>
+            <div className="d-flex align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <i className="bi bi-person-circle fs-4 me-2"></i>
+                <span className="fw-bold">rafa</span>
+              </div>
+              <Button
+                variant="outline-secondary"
+                size="sm"
+                className="d-flex align-items-center"
+              >
+                <i className="bi bi-box-arrow-right"></i>
+              </Button>
             </div>
-            <Button
-              variant="outline-secondary"
-              size="sm"
-              className="d-flex align-items-center"
-            >
-              <i className="bi bi-box-arrow-right"></i>
-            </Button>
           </div>
         </div>
       </div>
