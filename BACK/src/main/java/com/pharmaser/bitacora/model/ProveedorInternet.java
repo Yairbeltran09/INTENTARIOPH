4802320424c46177bcf87ae13ffb4d2c11ccc77f
Rafael Rojas R. <rafael312020@outlook.es>
2025-10-03 11:00:40 -0500

    update

--- BACK/src/main/java/com/pharmaser/bitacora/model/ProveedorInternet.java
+++ BACK/src/main/java/com/pharmaser/bitacora/model/ProveedorInternet.java
@@ -1,120 +1,120 @@
-package com.pharmaser.bitacora.model;
-import jakarta.persistence.*;
-import java.util.Date;
+    package com.pharmaser.bitacora.model;
+    import jakarta.persistence.*;
+    import java.util.Date;
 
-@Entity
-public class ProveedorInternet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Entity
+    public class ProveedorInternet {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
 
-    private String nombre;
-    private String nombre_contacto;
-    private Integer numero_contacto;
-    private Integer nit;
-    private String correo;
-    private String estado;
+        private String nombre;
+        private String nombre_contacto;
+        private Integer numero_contacto;
+        private Integer nit;
+        private String correo;
+        private String estado;
 
-    @Temporal(TemporalType.DATE)
-    private Date fecha_contratacion; // Corregido: usar minúsculas consistentemente
+        @Temporal(TemporalType.DATE)
+        private Date fecha_contratacion; // Corregido: usar minúsculas consistentemente
 
-    private String observacion; // Corregido: usar minúsculas consistentemente
-    private Boolean isDeleted = false;
+        private String observacion; // Corregido: usar minúsculas consistentemente
+        private Boolean isDeleted = false;
 
-    // Getters y Setters
-    public Boolean getIsDeleted() {
-        return isDeleted;
+        // Getters y Setters
+        public Boolean getIsDeleted() {
+            return isDeleted;
+        }
+
+        public void setIsDeleted(Boolean isDeleted) {
+            this.isDeleted = isDeleted;
+        }
+
+        public String getObservacion() {
+            return observacion;
+        }
+
+        public void setObservacion(String observacion) {
+            this.observacion = observacion;
+        }
+
+        public Date getFecha_contratacion() {
+            return fecha_contratacion;
+        }
+
+        public void setFecha_contratacion(Date fecha_contratacion) {
+            this.fecha_contratacion = fecha_contratacion;
+        }
+
+        public Integer getNumero_contacto() {
+            return numero_contacto;
+        }
+
+        public void setNumero_contacto(Integer numero_contacto) {
+            this.numero_contacto = numero_contacto;
+        }
+
+        public Integer getNit() {
+            return nit;
+        }
+
+        public void setNit(Integer nit) {
+            this.nit = nit;
+        }
+
+        public String getNombre_contacto() {
+            return nombre_contacto;
+        }
+
+        public void setNombre_contacto(String nombre_contacto) {
+            this.nombre_contacto = nombre_contacto;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getEstado() {
+            return estado;
+        }
+
+        public void setEstado(String estado) {
+            this.estado = estado;
+        }
+
+        public String getCorreo() {
+            return correo;
+        }
+
+        public void setCorreo(String correo) {
+            this.correo = correo;
+        }
+
+        @Override
+        public String toString() {
+            return "ProveedorInternet{" +
+                    "id=" + id +
+                    ", nombre='" + nombre + '\'' +
+                    ", nombre_contacto='" + nombre_contacto + '\'' +
+                    ", numero_contacto=" + numero_contacto +
+                    ", nit=" + nit +
+                    ", correo='" + correo + '\'' +
+                    ", estado='" + estado + '\'' +
+                    ", fecha_contratacion=" + fecha_contratacion +
+                    ", observacion='" + observacion + '\'' +
+                    ", isDeleted=" + isDeleted +
+                    '}';
+        }
     }
-
-    public void setIsDeleted(Boolean isDeleted) {
-        this.isDeleted = isDeleted;
-    }
-
-    public String getObservacion() {
-        return observacion;
-    }
-
-    public void setObservacion(String observacion) {
-        this.observacion = observacion;
-    }
-
-    public Date getFecha_contratacion() {
-        return fecha_contratacion;
-    }
-
-    public void setFecha_contratacion(Date fecha_contratacion) {
-        this.fecha_contratacion = fecha_contratacion;
-    }
-
-    public Integer getNumero_contacto() {
-        return numero_contacto;
-    }
-
-    public void setNumero_contacto(Integer numero_contacto) {
-        this.numero_contacto = numero_contacto;
-    }
-
-    public Integer getNit() {
-        return nit;
-    }
-
-    public void setNit(Integer nit) {
-        this.nit = nit;
-    }
-
-    public String getNombre_contacto() {
-        return nombre_contacto;
-    }
-
-    public void setNombre_contacto(String nombre_contacto) {
-        this.nombre_contacto = nombre_contacto;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    @Override
-    public String toString() {
-        return "ProveedorInternet{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", nombre_contacto='" + nombre_contacto + '\'' +
-                ", numero_contacto=" + numero_contacto +
-                ", nit=" + nit +
-                ", correo='" + correo + '\'' +
-                ", estado='" + estado + '\'' +
-                ", fecha_contratacion=" + fecha_contratacion +
-                ", observacion='" + observacion + '\'' +
-                ", isDeleted=" + isDeleted +
-                '}';
-    }
-}
